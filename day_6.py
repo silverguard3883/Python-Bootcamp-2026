714f8ed30742fe4e981105612d35fe3f9fdbeff0
silverguard3883 <silverguardgithub@gmail.com>
2026-02-22 16:41:37 -0700

    Add files via upload

---
+++ day_6.py
@@ -0,0 +1,55 @@
+#Functions
+
+#Many built-in functions make coding easier
+#Functions are a series of instructions used to perform a task repeatedly
+#Functions are followed by "()"
+
+print() #print is a function
+
+
+#"def" is a keyword that creates a function
+
+#def function():
+    #do something
+    #do something else
+    #finally, do this
+
+def my_function():  #The use of "def" is creating a function called "my_function". Everything after the colon is the operation the function will perform
+    print("Hello")
+    print("Goodbye")
+    
+my_function()
+
+
+#While Loops
+
+#While a condition is true, the while loop continues to run
+#While loops are better when there is an unknown number times a function must be performed or
+#when a condition needs to be met before the function stops running
+
+#while condition_is_true:
+    #do something
+    #keep doing something
+
+counter = 0
+while counter <= 10:
+    print(counter)
+    counter += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

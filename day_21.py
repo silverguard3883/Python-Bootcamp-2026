e257fb3df8689d5ef65c1b74af96ef1de78055d2
silverguard3883 <silverguardgithub@gmail.com>
2026-03-15 16:11:47 -0700

    Add files via upload

---
+++ day_21.py
@@ -0,0 +1,20 @@
+#Class inheritance
+#Classes can inherit attributes and methods from other classes
+
+"""
+class Subclass(Parent_Class):
+    def __init__(self):
+        super().__init__()          #"super" initializes everything in the parent class
+                                    #allows similar, but distinct classes to inherit attributes and methods without rewriting new classes
+
+    def subclass_method(self):
+        super().parent_class_method()   #Allows subclass to inherit ***specific*** method from parent class
+        print(stuff)
+
+"""
+
+
+
+
+
+

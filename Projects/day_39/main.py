dc4b163841a223e37c4a979c645b37eec18dc0fe
silverguard3883 <silverguardgithub@gmail.com>
2026-04-21 01:50:53 -0700

    Day 41 of Python Bootcamp

---
+++ Projects/day_39/main.py
@@ -0,0 +1,8 @@
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from flight_data import FlightData
+from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+import requests
+
+
